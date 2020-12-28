6965ec821440c04c56a6dc37585112316141ecb1
Amankaium <kaium@list.ru>
2020-12-28 17:58:47 +0600

    set static and media urls and roots

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 
 def homepage(request):
-    return HttpResponse("hello world!")
+    return render(request, "index.html")
 
 
 def test(request):
--- todo/urls.py
+++ todo/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage, test, second
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="home"),
     path("test/", test, name="test"),
     path("test2/", second)
-]
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
