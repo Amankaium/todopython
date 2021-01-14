83467bcefcf09a357803fe6a603c3f2f915a3913
Amankaium <kaium@list.ru>
2021-01-14 21:51:47 +0600

    added todo closing

--- main/views.py
+++ main/views.py
@@ -34,3 +34,10 @@ def mark_todo(request, id):
     todo.is_favorite = True
     todo.save()
     return redirect(test)
+
+
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
--- todo/urls.py
+++ todo/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
     path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("close-todo/<id>/", close_todo, name="close-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
