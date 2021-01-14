3f5cfe3f9976d544b6ba84ae4863123473c81cb0
Amankaium <kaium@list.ru>
2021-01-14 21:40:45 +0600

    added todo marking

--- main/views.py
+++ main/views.py
@@ -27,3 +27,10 @@ def delete_todo(request, id):
     todo = ToDo.objects.get(id=id)
     todo.delete()
     return redirect(test)
+
+
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test)
--- todo/urls.py
+++ todo/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path("test2/", second),
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
